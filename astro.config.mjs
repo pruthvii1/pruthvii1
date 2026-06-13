27c08f63c7da4c1f3d953965d56d4007ca133993
terrarix7 <terrarix7@gmail.com>
2026-06-13 16:31:45 +0530

    Fix Cloudflare static deployment

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,13 +1,8 @@
 // @ts-check
 import { defineConfig } from 'astro/config';
 
-import cloudflare from '@astrojs/cloudflare';
-
 // https://astro.build/config
 export default defineConfig({
-  adapter: cloudflare({
-    session: false,
-  }),
   devToolbar: {
     enabled: false
   }
